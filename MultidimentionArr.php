30fabd94ce72b23625bb592751d2b8af89f6b21b
rohankza <rohankza@gmail.com>
2025-02-12 19:32:16 +0530

    Update MultidimentionArr.php

--- MultidimentionArr.php
+++ MultidimentionArr.php
@@ -1,42 +1,13 @@
 <?php
-// $student=[
-//     "name" => "Dave Punk",
-//     "mob" => "5689741523",
-//     "email" => "davepunk@gmail.com",
-//     "collection"=>["book"=>'intro to ml',
-//                     'music'=>'rap',
-//                      array(1,2)
-//                   ]
-
-// ];
-
-
-
-
-// function arrahelper($values){
-
-// foreach($student as $keys=>$val){
-//     if(is_array($val)){
-//         echo $keys;
-//        arrahelper($val);
-//     }else{
-//         echo "$keys=>$val";
-//     };
-        
-//     };
-// };
-
-
-// arrahelper($student);
 
 
 
 
 
 $student = [
-    // "name" => "Dave Punk",
-    // "mob" => "5689741523",
-    // "email" => "davepunk@gmail.com",
+    "name" => "Dave Punk",
+    "mob" => "5689741523",
+    "email" => "davepunk@gmail.com",
     "collection" => [
         "book" => 'intro to ml',
         "music" => 'rap',
